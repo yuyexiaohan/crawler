0d62877522725590e3d3e3f553f6ca41a89dc4de
achjiang <htxz_jiang@163.com>
2018-12-10 15:09:42 +0800

    fix the bug of next_url

--- snbooks/snbooks/spiders/snbook.py
+++ snbooks/snbooks/spiders/snbook.py
@@ -2,6 +2,8 @@
 import scrapy
 # from snbooks.items import SnbooksItem
 from copy import deepcopy
+import re
+
 
 class SnbookSpider(scrapy.Spider):
     name = 'snbook'
@@ -16,7 +18,7 @@ class SnbookSpider(scrapy.Spider):
             item = {}
             item["cy_title"] = cy_list.xpath(".//a/text()").extract_first()
             item["cy_href"] = cy_list.xpath(".//a/@href").extract_first()
-            print("item['cy_href']:", item["cy_href"])
+            # print("item['cy_href']:", item["cy_href"])
             yield scrapy.Request(
                 item["cy_href"],
                 callback=self.parse_list,
@@ -25,22 +27,23 @@ class SnbookSpider(scrapy.Spider):
 
     def parse_list(self, response):
         """分类详情列表"""
-        print("---"*50)
+        print("---"*20)
         item = deepcopy(response.meta["item"])
         book_list = response.xpath("//div[@id='filter-results']/ul/li")
         for book in book_list:
-            item["book_price"] = book.xpath(".//div[@class='res-info']/p/em/text()").extract_first()
+            item["book_price"] = book.xpath(".//div[@class='res-info']/p/em/text()").extract_first()  # 价格是Ajax请求获取的，这里暂时存在bug,未修复
             item["book_desc"] = book.xpath(".//div[@class='res-info']/p[2]/a/text()").extract_first()
-            item["img_src"] = book.xpath(".//img/@src").extract_first()
+            item["img_src"] = book.xpath(".//img/@src2").extract_first()
             item["comment_nums"] = book.xpath(".//div[@class='res-info']/p[3]/a[1]/text()").extract_first()
             item["book_store"] = book.xpath(".//div[@class='res-info']/p[4]/a/text()").extract_first()
             item["book_href"] = book.xpath(".//div[@class='res-info']/p[2]/a/@href").extract_first()
-            print ("item:", item)
+            # print ("item:", item)
             yield item
-        next_url_part = response.xpath("//a[@id='nextPage']/@href").extract_first()
-        print("***"*50, next_url_part, "***"*50)
-        if next_url_part is not None:
-            next_url = "https://list.suning.com" + next_url_part
+        current_page = int ((re.findall (r"""param.currentPage = "(.*?)";""", response.body.decode())[0]))
+        page_numbers = int ((re.findall (r"""param.pageNumbers = "(.*?)";""", response.body.decode())[0]))
+        if current_page < page_numbers:
+            next_url_part = re.findall(r'^https.*-', item["cy_href"])[0]
+            next_url = next_url_part + str(current_page) + '.html'
             print("next_url:", next_url)
             yield scrapy.Request(
                 next_url,
