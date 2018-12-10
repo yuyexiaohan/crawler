3fdf87b46f43bf886d1005959d82ea76d273c915
achjiang <htxz_jiang@163.com>
2018-12-10 17:03:13 +0800

    修复了内容存在多余字符、空格的情况

--- crawlspider/crawlspider/pipelines.py
+++ crawlspider/crawlspider/pipelines.py
@@ -5,18 +5,32 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
+import re
 
 
 class CrawlspiderPipeline(object):
     def process_item(self, item, spider):
+        item["job_content"], item["job_request"] = self.process_content(item["job_content"], item["job_request"])
         self.save_file(item)
         # print("item:", item)
         return item
 
     def save_file(self, item):
         """存储爬去文件"""
-        file_path = "./tencent_hr.text"
+        file_path = "./tencent_hr.txt"
         with open (file_path, "a", encoding="utf-8") as f:
             f.write (json.dumps (dict(item), ensure_ascii=False, indent=2))
             f.write ("\n")
         print ("保存成功！")
+
+    def process_content(self, job_content, job_request):
+        """处理文档内容"""
+        job_content = self.contet_manage(job_content)
+        job_request = self.contet_manage(job_request)
+        return job_content, job_request
+
+    def contet_manage(self, li):
+        li = [re.sub (r"\r|\s+", "", i) for i in li]
+        li = [i for i in li if len (i) > 0]
+        return li
+
--- crawlspider/crawlspider/spiders/tencent.py
+++ crawlspider/crawlspider/spiders/tencent.py
@@ -7,10 +7,17 @@ from scrapy.spiders import CrawlSpider, Rule
 class TencentSpider(CrawlSpider):
     name = 'tencent'
     allowed_domains = ['tencent.com']
-    start_urls = ['https://hr.tencent.com/position.php?&start=0#a']
+    start_urls = ['https://hr.tencent.com/position.php?&start=0#a']  # 当前起始页
 
     rules = (
+        # url规则，
+        # LinkExtractor：链接提取器，
+        # allow：允许爬去的url,按照实际网页情况填写，框架回自动补全
+        # callback: 调用具体的函数执行，该url界面的信息提取
+        # follow：是否跟随循环
+        # 1.详情页面：
         Rule (LinkExtractor (allow=r'position_detail\.php\?id=\d+&keywords=&tid=\d&lid=\d'), callback='parse_item'),
+        # 2.翻页部分
         Rule(LinkExtractor(allow=r'position\.php\?&start=\d+#a'), follow=True),
         # Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
@@ -24,7 +31,7 @@ class TencentSpider(CrawlSpider):
         # item["publish_date"] = response.xpath ("./td[5]/text()").extract_first()
         item["job_content"] = response.xpath ("//table[@class='tablelist textl']//tr[3]//text()").extract()
         item["job_request"] = response.xpath ("//table[@class='tablelist textl']//tr[4]//text()").extract()
-        print(item)
+        # print(item)
         return item
 
 
