4ccd72bed29f2cb8a9fe00a3874632108791fc1d
achjiang <htxz_jiang@163.com>
2018-12-11 17:08:18 +0800

    修改文件位置

---
+++ requests/douban/settings.py
@@ -0,0 +1,15 @@
+# coding=utf-8 
+# Time : 2018/12/5
+# Author : achjiang
+
+"""参数配置"""
+# 1.豆瓣登录信息
+EMAIL = "369668247@qq.com"
+PASSWORD = "QAZwsx1992,.DB"
+
+# 2.云打码登录信息
+# 用户名
+YDM_USERNAME = 'achjiang'
+
+# 密码
+YDM_PASSWORD = 'QAZwsx1992,.YY'
