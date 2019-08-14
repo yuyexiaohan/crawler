49515866a8a7ff49d2095095d6968702f32eb6d6
achjiang <htxz_jiang@163.com>
2019-08-14 21:19:25 +0800

    Update settings.py
    del pw

--- requests/douban/settings.py
+++ requests/douban/settings.py
@@ -4,12 +4,12 @@
 
 """参数配置"""
 # 1.豆瓣登录信息
-EMAIL = "369668247@qq.com"
-PASSWORD = "QAZwsx1992,.DB"
+EMAIL = "XXXXX@qq.com"
+PASSWORD = "XXXXX"
 
 # 2.云打码登录信息
 # 用户名
-YDM_USERNAME = 'achjiang'
+YDM_USERNAME = 'XXXX'
 
 # 密码
-YDM_PASSWORD = 'QAZwsx1992,.YY'
+YDM_PASSWORD = 'XXXXX'
